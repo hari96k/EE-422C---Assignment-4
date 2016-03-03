6c180c6db4eddd814fb0319c5db82fe3757510b7
kevinjyee <kevjyee@gmail.com>
2016-03-02 22:51:25 -0600

    Updated Junit Test Cases

--- assignment4/WordLadderTest.java
+++ assignment4/WordLadderTest.java
@@ -2,16 +2,20 @@ package assignment4;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 import org.junit.Test;
 
 public class WordLadderTest {
 
+	
 	@Test
-	public void test() throws NoSuchLadderException 
+	public void test1() throws NoSuchLadderException 
 		{
-		
 		Assignment4Interface wordLadderSolver = new WordLadderSolver();
 		
 		List<String> result = wordLadderSolver.computeLadder("angel","devil");
@@ -20,19 +24,28 @@ public class WordLadderTest {
 	
 	    }
 	
-	
-	public void test1() throws NoSuchLadderException 
+	@Test
+	public void test() throws NoSuchLadderException 
 	{
-	
+	 ArrayList<String> Dictionary = new ArrayList<String>();
+	 Dictionary.add("heads");
+	 Dictionary.add("heals");
+	 Dictionary.add("veals");
+	 Dictionary.add("veils");
+	 Dictionary.add("vails");
+	 Dictionary.add("tails");
 	Assignment4Interface wordLadderSolver = new WordLadderSolver();
-	
-	List<String> result = wordLadderSolver.computeLadder("money","smart");
-	
-	boolean correct = wordLadderSolver.validateResult("money","smart",result);
+	wordLadderSolver.setDictionary(Dictionary);
+	List<String> result = wordLadderSolver.computeLadder("heads","tails");
+	Collections.reverse(result);
+	boolean correct = wordLadderSolver.validateResult("heads","tails",result);
 	assertTrue(correct);
 
     }
+	
+	   
 		
 	}
+
 
 
