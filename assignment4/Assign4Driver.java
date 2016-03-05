c9cd07024162e89c73137e1072aecbb969bc331e
Hari Krishna Kosuru <hari96k@utexas.edu>
2016-03-04 23:21:00 -0600

    Added corner case handling, reverted to original input format

--- assignment4/Assign4Driver.java
+++ assignment4/Assign4Driver.java
@@ -1,11 +1,9 @@
 /*
- Assign4Driver Class
+ WordLadder Solver Class
  Solves EE422C programming assignment #4
- @author Hari (), Kevin Yee (kjy252)
- @version 1.00 2016-02-025
+ @author Hari (hk8663), Kevin Yee (kjy252)
+ @version 1.01 2016-03-03
  */
- 
-
 
 package assignment4;
 
@@ -16,126 +14,143 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ArrayList;
+
+public class Assign4Driver {
+
+	public static void main(String[] args) throws Exception {
+		if (args.length != 2) {
+			System.err.println("Error: Incorrect number of command line arguments");
+			System.exit(-1);
+		}
+		processDictionary(args[0]);					// args[0] contains dictionary			
+		processCommands(args[1]);					// args[1] contains test file
+
+	}
 
 
-public class Assign4Driver
-{
-	public static  ArrayList<String> wordDictionary;
-	
-	/*Function: processCommands
-     * *****************************
-     * Process word pairs to solve
-     * @params file name
-     ******************************/	
-	public static void processCommands (String filename) throws Exception 
-    {
-        try 
-        {
-            // Read file 
-            FileReader freader = new FileReader(filename);
-            BufferedReader reader = new BufferedReader(freader);
-            
-            // Iterate over and process each line of file
-            for (String s = reader.readLine(); s != null; s = reader.readLine()) 
-            {
-                String[] inputs = new String[2];
-                inputs = s.split("\\s+");							
-                solveLadder(inputs); //solves each word ladder
-            } 
-        }
-            
-        catch (FileNotFoundException e) 
-        {
-            System.err.println ("Error: File not found. Exiting...");
-            e.printStackTrace();
-            System.exit(-1);
-        } catch (IOException e) 
-        {
-            System.err.println ("Error: IO exception. Exiting...");
-            e.printStackTrace();
-            System.exit(-1);
-        }
-        return;
-    }
-	    
-    
-	/*Function: processDictionary
-     * *****************************
-     * Process word pairs to solve
-     * @params fileName
-     ******************************/ 
-	public static void processDictionary (String filename) throws Exception 
-    {
-      Dictionary wordDict = new Dictionary();
-      wordDictionary = wordDict.getDictionary(filename);
-      return;
-    }
-	
-    /*Function: solveLadder
-     * *****************************
-     * Solves the WordLadder 
-     * @param inputs  Strings to build ladder from
-     * @return void
-     ******************************/  
-    public static void solveLadder(String[] inputs)
-    {
-        Assignment4Interface wordLadderSolver = new WordLadderSolver();
 
-        try 
-        {
-        	wordLadderSolver.setDictionary(wordDictionary);
-            List<String> result = wordLadderSolver.computeLadder(inputs[0], inputs[1]);
-            if(result != null){printResult(result);}
-    
-            boolean correct = true;
-            if(result != null){correct = wordLadderSolver.validateResult(inputs[0], inputs[1], result);}
-     
-            if(!correct){System.out.println("Wrong");}
-        } 
-        catch (NoSuchLadderException e) 
-        {
-            e.printStackTrace();
-        }
-    }
-    
-    /* Function: printResult
-     * ****************************
-     * Prints Item in Results Array
-     * @param result    result of Word Ladder
-     * @return void
-     ******************************/  
-    
-    public static void printResult(List<String> result)
-    {
-    	Collections.reverse(result);
-    	Iterator<String> i = result.iterator();
-        while (i.hasNext())
-        {
-            String temp = i.next();  // grab next item                                                         
-            System.out.println(temp);
-        }
-        
-        System.out.println("**********"); 
-     
-    }
-    
-    
-    /* Function: main
-     * ****************************
-     * main inputs
-     ******************************/  
-    public static void main(String[] args) throws Exception
-    {
-            if (args.length != 2)
-            {
-                System.err.println ("Error: Incorrect number of command line arguments");
-                System.exit(-1);
-            }
-            processDictionary (args[0]);//dictionary in arg 0
-            processCommands(args[1]);//commands in arg 1
+	/* Function: processDictoinary
+	 * 		Puts Dictionary into ArrayList
+	 */
+	public static void processDictionary(String filename) throws Exception {
+		BufferedReader reader = null;		// Outside try block to ensure that reader is closed in the finally block
+		try {
+			// Read file
+			FileReader freader = new FileReader(filename);
+			reader = new BufferedReader(freader);
 
-    }
-    
-   
+			// Iterate over and process each line of file
+			for (String s = reader.readLine(); s != null; s = reader.readLine()) {
+				if (!s.startsWith("*")) {
+					WordLadderSolver.getDictionary().add(s.substring(0, 5)); // add words to dictionary
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("Error: File not found. Exiting...");
+			e.printStackTrace();
+			System.exit(-1);
+		} catch (IOException e) {
+			System.err.println("Error: IO exception. Exiting...");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		finally {
+			if (reader != null) 
+				reader.close();
+		}
+		return;
+	}
+
+	/* Function: processCommands 
+	 * 		Process word pairs to solve
+	*/
+
+	public static void processCommands(String filename) throws Exception {
+		BufferedReader reader = null;		// Outside try block to ensure that reader is closed in the finally block
+		try {
+			// Read file
+			FileReader freader = new FileReader(filename);
+			reader = new BufferedReader(freader);
+
+			// Iterate over and process each line of file
+			for (String s = reader.readLine(); s != null; s = reader.readLine()) {
+				String[] inputs = new String[2];
+				inputs = s.split("\\s+");
+				solveLadder(inputs); // solves each word ladder
+			}
+		}
+
+		catch (FileNotFoundException e) {
+			System.err.println("Error: File not found. Exiting...");
+			e.printStackTrace();
+			System.exit(-1);
+		} catch (IOException e) {
+			System.err.println("Error: IO exception. Exiting...");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		finally {
+			if (reader != null) 
+				reader.close();
+		}
+		return;
+	}
+
+	/*
+	 * Function: solveLadder ***************************** Solves the WordLadder
+	 * 
+	 * @param inputs Strings to build ladder from
+	 * 
+	 * @return void
+	 ******************************/
+	public static void solveLadder(String[] inputs) {
+		Assignment4Interface wordLadderSolver = new WordLadderSolver();
+
+		try {
+			String startWord = inputs[0].toLowerCase();
+			String endWord = inputs[1].toLowerCase();
+			if (!WordLadderSolver.getDictionary().contains(startWord) || !WordLadderSolver.getDictionary().contains(endWord)){
+				System.out.println("For the input words " + startWord + " and " +endWord + ":\nAt least one of the words " + startWord + " and " + endWord + " are not legitimate 5-letter words from the dictionary");
+				return;
+			}
+			
+			List<String> result = wordLadderSolver.computeLadder(startWord, endWord);
+			if (result != null) {
+				printResult(result);
+			}
+
+			boolean correct = true;
+			if (result != null) {
+				correct = wordLadderSolver.validateResult(inputs[0], inputs[1], result);
+			}
+			if (!correct) {
+				System.out.println("Wrong");
+			}
+		} catch (NoSuchLadderException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * Function: printResult **************************** Prints Item in Results
+	 * Array
+	 * 
+	 * @param result result of Word Ladder
+	 * 
+	 * @return void
+	 ******************************/
+
+	public static void printResult(List<String> result) {
+		Collections.reverse(result);
+		System.out.println("For the input words " + result.get(0) + " and " + result.get(result.size()-1) + ", the following word ladder was found:");
+		Iterator<String> i = result.iterator();
+		while (i.hasNext()) {
+			String temp = i.next(); // grab next item
+			System.out.println(temp);
+		}
+
+		System.out.println("**********");
+
+	}
+
 }
--- assignment4/WordLadderSolver.java
+++ assignment4/WordLadderSolver.java
@@ -13,69 +13,87 @@ import java.util.*;
 // do not change class name or interface it implements
 public class WordLadderSolver implements Assignment4Interface {
 
-	private ArrayList<String> dictionary = new ArrayList<String>();
+	private static ArrayList<String> Dictionary = new ArrayList<String>();
 	private static ArrayList<String> SolutionsList = new ArrayList<String>();
 	private static char[] eachLetter = new char[5];
 
 	/*
-	 * Function: computeLadder 
-	 * *****************************
-	 *Checks if there's an obvious path
+	 * Function: setLetter ***************************** Turns start into
+	 * individual chars to check
+	 * 
+	 * @params start
+	 * 
+	 * @return void
+	 ******************************/
+
+	private void setLetter(String start) {
+		eachLetter[0] = start.charAt(0);
+		eachLetter[1] = start.charAt(1);
+		eachLetter[2] = start.charAt(2);
+		eachLetter[3] = start.charAt(3);
+		eachLetter[4] = start.charAt(4);
+	}
+
+	/*
+	 * Function: computeLadder ***************************** Find all Paths
+	 * Using BFS
+	 * 
 	 * @params startWord
+	 * 
 	 * @params endWord
+	 * 
 	 * @return result List
 	 ******************************/
-	public List<String> computeLadder(String startWord, String endWord) throws NoSuchLadderException
-	{
+	public List<String> computeLadder(String startWord, String endWord) throws NoSuchLadderException {
+
 		
-		if(baseCheck(startWord,endWord))
-		{
+		/* If the String is not of length 5 return error */
+		if (startWord.length() != 5 && endWord.length() != 5) {
+			printError(startWord, endWord);
 			return null;
 		}
+		
 
 		if (diffbyOne(startWord, endWord)) {
 			SolutionsList.add(endWord);
 			SolutionsList.add(startWord);
 			return SolutionsList;
 		}
-		return searchWord(startWord,endWord);
-	}
-	
-	
-	/*
-	 * Function: searchWord
-	 * *****************************
-	 * Find all Paths Using BFS
-	 * @params startWord
-	 * @params endWord
-	 * @return result List
-	 ******************************/
-	 public List<String> searchWord (String startWord,String endWord){
+		
+		if (startWord.equals(endWord)) {
+			SolutionsList.add(endWord);
+			SolutionsList.add(startWord);
+			return SolutionsList;
+		}
+
 		HashMap<String, Integer> ladder = new HashMap<>(); // ladder from one word to another
 		HashMap<String, ArrayList<String>> graph = new HashMap<>(); // graph of all possibilities
 		Queue<String> Q = new LinkedList<String>();
+
 		ladder.put(startWord, 1);
 		Q.add(startWord);
+
 		// BFS
 		while (!Q.isEmpty()) {
 			String word = Q.remove();
 			setLetter(word); // allows for easier conversions of char
 
 			if (word.equals(endWord)) {
-			 //we've found our word and can exit
+				// minSteps = ladder.get(word); //we've found our word and can exit
 				break;
 			}
 
 			int count = ladder.get(word) + 1; // one step from each previous word. keeps track of duplicates
-			String tempString = new String();
+			// if(count>minSteps){break;}
+
 			for (int i = 0; i < 5; i++) {
 				char[] temp = new char[5];
 				temp = Arrays.copyOf(eachLetter, 5);
 				for (char c = 'a'; c <= 'z'; c++) {
 					/* Iterate through ever possible differOne combos */
 					temp[i] = c;
-					tempString = String.copyValueOf(temp);
-					if (!dictionary.contains(tempString)) {
+					String tempString = new String(temp);
+					if (!Dictionary.contains(tempString)) {
 						continue;
 					} // if not in dictionary, ignore
 
@@ -96,11 +114,8 @@ public class WordLadderSolver implements Assignment4Interface {
 			}
 		}
 
-		//create our result
 		ArrayList<String> result = new ArrayList<>();
-		
 		if (!graph.containsKey(endWord)) {
-			//if not in the graph return null
 			printError(startWord, endWord);
 			return null;
 		}
@@ -110,39 +125,20 @@ public class WordLadderSolver implements Assignment4Interface {
 		SolutionsList = result;
 		return SolutionsList;
 	}
-	/*
-	 * Fuction:baseCheck
-	 * *******************************
-	 * checks if base case met
-	 ********************************/
-	
-	private boolean baseCheck(String startWord, String endWord)
-	{
-		if(startWord == null || endWord == null){
-			return true;
-		}
-		/* If the String is not of length 5 return error */
-		if (startWord.length() != 5 && endWord.length() != 5) {
-			printError(startWord, endWord);
-			return true;
-		}
-		if(!dictionary.contains(startWord) || !dictionary.contains(endWord)){
-			return true;
-		}
-		
-		return false;
-	}
-	
-	
+
 	public boolean found = false;
 	/*
-	 * Function: buildResult 
-	 * ***************************** 
-	 * buildResult from BFS using DFS
+	 * Function: buildResult ***************************** buildResult from BFS
+	 * using DFS
+	 * 
 	 * @params end
+	 * 
 	 * @params startWord
+	 * 
 	 * @params result
+	 * 
 	 * @params graph
+	 * 
 	 * @return result List
 	 ******************************/
 
@@ -175,31 +171,7 @@ public class WordLadderSolver implements Assignment4Interface {
 		}
 		return result;
 	}
-	
-	/*
-	 * Function: setLetter
-	 *  *****************************
-	 *  Turns start into individual chars to check
-	 * @params start
-	 * @return void
-	 ******************************/
-	private void setLetter(String start) {
-		eachLetter[0] = start.charAt(0);
-		eachLetter[1] = start.charAt(1);
-		eachLetter[2] = start.charAt(2);
-		eachLetter[3] = start.charAt(3);
-		eachLetter[4] = start.charAt(4);
-	}
-
-
-
-	/*
-	 * Function: validateResult
-	 * ***************************** 
-	 * validates that the solution is correct
-	 * @params endword
-	 * @params startWord
-	 ******************************/
+	// implement this method
 
 	@Override
 	public boolean validateResult(String startWord, String endWord, List<String> wordLadder) {
@@ -218,15 +190,6 @@ public class WordLadderSolver implements Assignment4Interface {
 		return true;
 	}
 
-	
-	/*
-	 * Function: diffbyOne
-	 * ***************************** 
-	 * determines is the strings diff by one
-	 * @params str1
-	 * @params str2
-	 ******************************/
-	
 	private boolean diffbyOne(String str1, String str2) {
 
 		if (str1.length() != str2.length())
@@ -239,26 +202,18 @@ public class WordLadderSolver implements Assignment4Interface {
 		return same == str1.length() - 1;
 	}
 
-	/*
-	 * Function: printError
-	 * ***************************** 
-	 * prints Error if string is incorrect
-	 * @params startWord
-	 * @params endWord
-	 ******************************/	
-
 	private void printError(String startWord, String endWord) {
 		System.out.println("No path found between " + startWord + " and " + endWord);
 		System.out.println("**********");
 		return;
 	}
 
-	public ArrayList<String> getDictionary() {
-		return this.dictionary;
+	public static ArrayList<String> getDictionary() {
+		return Dictionary;
 	}
 
 	public void setDictionary(ArrayList<String> dictionary) {
-		this.dictionary = dictionary;
+		Dictionary = dictionary;
 	}
 
 }
